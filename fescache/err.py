2b300c04ff38842784690f71a75da6427aebc3db
guoyanfeng <guo.yanfeng@dataeveryday.com>
2020-09-08 17:38:49 +0800

    去掉httperror异常类
    Signed-off-by: guoyanfeng <guo.yanfeng@dataeveryday.com>

--- fescache/err.py
+++ fescache/err.py
@@ -7,7 +7,7 @@
 @time: 18-12-25 下午2:08
 """
 
-__all__ = ("Error", "HttpError", "RedisClientError", "RedisConnectError", "FuncArgsError", "RedisTimeoutError")
+__all__ = ("Error", "RedisClientError", "RedisConnectError", "FuncArgsError", "RedisTimeoutError")
 
 
 class Error(Exception):
@@ -23,23 +23,6 @@ class Error(Exception):
 
     def __repr__(self):
         return "<{} '{}'>".format(self.__class__.__name__, self.message)
-
-
-class HttpError(Error):
-    """
-    主要处理http 错误,从接口返回
-    """
-
-    def __init__(self, status_code, *, message=None, error=None):
-        self.status_code = status_code
-        self.message = message
-        self.error = error
-
-    def __str__(self):
-        return "{}, '{}':'{}'".format(self.status_code, self.message, self.message or self.error)
-
-    def __repr__(self):
-        return "<{} '{}: {}'>".format(self.__class__.__name__, self.status_code, self.error or self.message)
 
 
 class RedisClientError(Error):
